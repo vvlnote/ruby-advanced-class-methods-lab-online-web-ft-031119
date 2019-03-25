dbc7df34a0f698bfbf73d51b440c3dbc475119a5
Joy Lin <vvlnote@hotmail.com>
2019-03-25 17:59:56 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -30,7 +30,7 @@ class Song
   
   def self.new_from_filename(file_name)
     arr = file_name.split(/"-"|\./)
-    song = self_by_name(arr[1])
+    song = self.new_by_name(arr[1])
     song.artist_name = arr[0]
     song
   end
